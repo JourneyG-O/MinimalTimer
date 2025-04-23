37b8cf4b527c430769ad021fce4de9414eb8cfe2
JourneyG-O <gojs2639@gmail.com>
2025-04-23 17:20:03 +0900

    ✨ feat(model): define TimerModel with core properties

---
+++ MinimalTimer/MinimalTimer/Models/TimerModel.swift
@@ -0,0 +1,16 @@
+//
+//  TimerModel.swift
+//  MinimalTimer
+//
+//  Created by KoJeongseok on 4/23/25.
+//
+
+import SwiftUI
+
+struct TimerModel: Identifiable {
+    let id = UUID()
+    var title: String
+    var totalTime: TimeInterval
+    var currentTime: TimeInterval
+    var color: Color
+}
