46c145fc83d680a712e27ef45c2cd0637f54ea1b
JourneyG-O <gojs2639@gmail.com>
2025-04-23 17:05:36 +0900

    🎉 init: create MinimalTimer project with SwiftUI

---
+++ MinimalTimer/MinimalTimer/MinimalTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalTimerApp.swift
+//  MinimalTimer
+//
+//  Created by KoJeongseok on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct MinimalTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
