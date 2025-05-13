c69034a31290ea0b1b952aa3467382fe5a79fba7
JourneyG-O <gojs2639@gmail.com>
2025-05-13 17:21:01 +0900

    ✨ feat(view): create RemainingTimeView to display formatted timer text

---
+++ MinimalTimer/MinimalTimer/Views/RemainingTimeView.swift
@@ -0,0 +1,24 @@
+//
+//  RemainingTimeView.swift
+//  MinimalTimer
+//
+//  Created by KoJeongseok on 5/13/25.
+//
+
+import SwiftUI
+
+struct RemainingTimeView: View {
+    @ObservedObject var viewModel: MainViewModel
+
+    var body: some View {
+        Text(formatTime(viewModel.currentTimer?.currentTime ?? 0))
+            .font(.system(size: 48, weight: .bold, design: .monospaced))
+    }
+
+    func formatTime(_ timeInterval: TimeInterval) -> String {
+        let minutes = Int(timeInterval) / 60
+        let seconds = Int(timeInterval.truncatingRemainder(dividingBy: 60))
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+}
+
