7f3542ad4a208e4811ce2a21ebfc19d044e7dcc3
JourneyG-O <gojs2639@gmail.com>
2025-05-25 19:08:04 +0900

    🔧 chore: Add TimerStyleType enum for timer appearance customizatio

---
+++ MinimalTimer/MinimalTimer/Models/TimerStyleType.swift
@@ -0,0 +1,14 @@
+//
+//  TimerStyleType.swift
+//  MinimalTimer
+//
+//  Created by KoJeongseok on 5/25/25.
+//
+
+import Foundation
+
+enum TimerStyleType {
+    case neumorphic
+    case flat
+    case dial
+}
--- MinimalTimer/MinimalTimer/Views/TimerContnetView.swift
+++ MinimalTimer/MinimalTimer/Views/TimerContnetView.swift
@@ -10,12 +10,7 @@ import SwiftUI
 struct TimerContentView: View {
     let timer: TimerModel
     let progress: CGFloat
-    let isInteractive: Bool
-
-    // Interaction 콜백
-    let onSingleTap: (() -> Void)?
-    let onDoubleTap: (() -> Void)?
-    let onDrag: ((Double) -> Void)?
+    let size: CGFloat
 
     var body: some View {
         GeometryReader { geometry in
@@ -27,41 +22,9 @@ struct TimerContentView: View {
                 PieShape(progress: progress)
                     .fill(timer.color)
 
-                // 상호작용 레이어 (재사용 가능한 뷰 블럭으로 분리)
-                interactionLayer(center: center)
             }
             .frame(width: size, height: size)
             .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
-        }
-    }
-
-    // 터치 / 드래그 입력을 처리하는 레이어
-    @ViewBuilder
-    private func interactionLayer(center: CGPoint) -> some View {
-        let baseCircle = Circle()
-            .fill(Color.clear)
-            .contentShape(Circle())
-
-        if isInteractive {
-            baseCircle
-                .gesture(
-                    DragGesture()
-                        .onChanged { value in
-                            let angle = center.angle(to: value.location)
-                            onDrag?(angle)
-                        }
-                )
-                .onTapGesture(count: 1) {
-                    onSingleTap?()
-                }
-                .onTapGesture(count: 2) {
-                    onDoubleTap?()
-                }
-        } else {
-            baseCircle
-                .onTapGesture {
-                    onSingleTap?()
-                }
         }
     }
 }
