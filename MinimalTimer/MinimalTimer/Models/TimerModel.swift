36f6a4f783ee8fc775f28e69591b638222725b7f
JourneyG-O <gojs2639@gmail.com>
2025-06-20 17:06:43 +0900

    ✨ feat: Add TimerStore with persistence using UserDefaults and DTO conversion

--- MinimalTimer/MinimalTimer/Models/TimerModel.swift
+++ MinimalTimer/MinimalTimer/Models/TimerModel.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct TimerModel: Identifiable {
-    let id = UUID()
+    let id: UUID
     var title: String
     var totalTime: TimeInterval
     var currentTime: TimeInterval
     var color: Color
+
+    init(
+        id: UUID = UUID(), // ← 기본값 설정
+        title: String,
+        totalTime: TimeInterval,
+        currentTime: TimeInterval,
+        color: Color
+    ) {
+        self.id = id
+        self.title = title
+        self.totalTime = totalTime
+        self.currentTime = currentTime
+        self.color = color
+    }
 }
---
+++ MinimalTimer/MinimalTimer/Models/TimerStore.swift
@@ -0,0 +1,114 @@
+//
+//  TimerStore.swift
+//  MinimalTimer
+//
+//  Created by KoJeongseok on 6/20/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct TimerModelDTO: Codable {
+    var id: UUID
+    var title: String
+    var totalTime: TimeInterval
+    var currentTime: TimeInterval
+    var colorHex: String
+}
+
+extension TimerModel {
+    func toDTO() -> TimerModelDTO {
+        TimerModelDTO(
+            id: id,
+            title: title,
+            totalTime: totalTime,
+            currentTime: currentTime,
+            colorHex: color.toHex()
+        )
+    }
+}
+
+extension TimerModelDTO {
+    func toModel() -> TimerModel {
+        TimerModel(
+            id: id,
+            title: title,
+            totalTime: totalTime,
+            currentTime: currentTime,
+            color: Color(hex: colorHex)
+        )
+    }
+}
+
+class TimerStore {
+    private let timerKey = "savedTimers"
+    private let selectedIndexKey = "selectedTimerIndex"
+
+    func save(timers: [TimerModel], selectedIndex: Int) {
+        let dtos = timers.map { $0.toDTO() }
+        if let encoded = try? JSONEncoder().encode(dtos) {
+            UserDefaults.standard.set(encoded, forKey: timerKey)
+        }
+        UserDefaults.standard.set(selectedIndex, forKey: selectedIndexKey)
+    }
+
+    func load() -> ([TimerModel], Int) {
+        var timers: [TimerModel] = []
+        var index: Int = 0
+
+        if let data = UserDefaults.standard.data(forKey: timerKey),
+           let decoded = try? JSONDecoder().decode([TimerModelDTO].self, from: data) {
+            timers = decoded.map { $0.toModel() }
+        } else {
+            timers = TimerStore.defaultTimers
+        }
+
+        index = UserDefaults.standard.integer(forKey: selectedIndexKey)
+
+        return (timers, index)
+    }
+
+    static var defaultTimers: [TimerModel] {
+        return [
+            TimerModel(title: "표준 타이머", totalTime: 60 * 60, currentTime: 0, color: .yellow),
+            TimerModel(title: "긴 타이머", totalTime: 120 * 60, currentTime: 0, color: .blue),
+            TimerModel(title: "짧은 타이머", totalTime: 5 * 60, currentTime: 0, color: .red)
+        ]
+    }
+}
+
+// Color -> Hex 변환 유틸리티
+extension Color {
+    func toHex() -> String {
+        UIColor(self).toHex() ?? "#000000"
+    }
+
+    init(hex: String) {
+        self = Color(UIColor(hex: hex))
+    }
+}
+
+// UIColor 변환 유틸리티 (iOS용)
+extension UIColor {
+    convenience init(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+        let r = CGFloat((rgb & 0xFF0000) >> 16) / 255
+        let g = CGFloat((rgb & 0x00FF00) >> 8) / 255
+        let b = CGFloat(rgb & 0x0000FF) / 255
+
+        self.init(red: r, green: g, blue: b, alpha: 1.0)
+    }
+
+    func toHex() -> String? {
+        var r: CGFloat = 0, g: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+
+        let rgb: Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        return String(format: "#%06x", rgb)
+    }
+}
