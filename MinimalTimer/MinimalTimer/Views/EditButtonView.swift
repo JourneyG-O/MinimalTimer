e2a4bd9a20486ec918350c41c02f26c79e759000
JourneyG-O <gojs2639@gmail.com>
2025-08-04 17:58:56 +0900

    ✨ feat: Add EditButtonView for switching mode

---
+++ MinimalTimer/MinimalTimer/Views/EditButtonView.swift
@@ -0,0 +1,22 @@
+//
+//  EditButtonView.swift
+//  MinimalTimer
+//
+//  Created by KoJeongseok on 8/4/25.
+//
+
+import SwiftUI
+
+struct EditButtonView: View {
+    var action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            Image(systemName: "gearshape.fill")
+                .font(.system(size: 28))
+                .padding()
+                .background(Circle().fill(Color(.secondarySystemBackground)))
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+}
