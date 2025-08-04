9df20f37820aeff7a5fa6aa1779536d54b758624
JourneyG-O <gojs2639@gmail.com>
2025-08-05 00:20:16 +0900

    ♻️ refactor: Separate RemainingTimeView to only show current time

--- MinimalTimer/MinimalTimer/Views/PreviewTimerView.swift
+++ MinimalTimer/MinimalTimer/Views/PreviewTimerView.swift
@@ -9,11 +9,16 @@ import SwiftUI
 
 struct PreviewTimerView: View {
     let color: Color
+//    let totalTime: TimeInterval
     let onTap: () -> Void
 
     var body: some View {
         Circle()
             .fill(color)
+            .overlay(
+                Circle()
+                    .stroke(Color.primary.opacity(0.2), lineWidth: 2)
+            )
             .onTapGesture(perform: onTap)
     }
 }
