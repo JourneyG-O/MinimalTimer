bcf6aa21e106832b6ec2906b8e3df27b4385a490
JourneyG-O <gojs2639@gmail.com>
2025-08-05 00:54:31 +0900

    🎨 style: Remove opacity modifier from time label in PreviewTimerView

--- MinimalTimer/MinimalTimer/Views/PreviewTimerView.swift
+++ MinimalTimer/MinimalTimer/Views/PreviewTimerView.swift
@@ -19,7 +19,6 @@ struct PreviewTimerView: View {
 
             Text(formatTime(totalTime))
                 .font(.system(size: 40, weight: .bold, design: .rounded))
-                .opacity(0.5)
         }
         .onTapGesture(perform: onTap)
     }
