a51af8a4eae178693fc05c1a9946c5b8507c9f2e
JourneyG-O <gojs2639@gmail.com>
2025-08-11 15:54:25 +0900

    ✨ feat: Add Draft↔Model mapping for Timer

--- MinimalTimer/MinimalTimer/Models/TimerModel.swift
+++ MinimalTimer/MinimalTimer/Models/TimerModel.swift
@@ -26,8 +26,8 @@ struct TimerModel: Identifiable {
         color: Color,
         isTickAlwaysVisible: Bool = false,
         isVibrationEnabled: Bool = true,
-        isSoundEnaved: Bool = true,
-        isRepeatEnaved: Bool = false
+        isSoundEnabled: Bool = true,
+        isRepeatEnabled: Bool = false
 
     ) {
         self.id = id
@@ -37,7 +37,47 @@ struct TimerModel: Identifiable {
         self.color = color
         self.isTickAlwaysVisible = isTickAlwaysVisible
         self.isVibrationEnabled = isVibrationEnabled
-        self.isSoundEnabled = isSoundEnaved
-        self.isRepeatEnabled = isRepeatEnaved
+        self.isSoundEnabled = isSoundEnabled
+        self.isRepeatEnabled = isRepeatEnabled
+    }
+}
+
+extension TimerDraft {
+    // TimerModel -> TimerDraft (편집하기로 진입 시 사용)
+    init(model: TimerModel) {
+        self.title = model.title
+        self.color = model.color
+        self.totalSeconds = Int(model.totalTime)
+        self.isTickAlwaysVisible = model.isTickAlwaysVisible
+        self.isVibrationEnabled = model.isVibrationEnabled
+        self.isSoundEnabled = model.isSoundEnabled
+        self.isRepeatEnabled = model.isRepeatEnabled
+    }
+}
+
+extension TimerModel {
+    // Draft -> TimerModel 생성하기로 진입 시 사용 (타이머 새로 만들 때)
+    init(from draft: TimerDraft) {
+        self.id = UUID()
+        self.title = draft.title
+        self.color = draft.color
+        self.totalTime = TimeInterval(draft.totalSeconds)
+        self.currentTime = TimeInterval(draft.totalSeconds)
+        self.isTickAlwaysVisible = draft.isTickAlwaysVisible
+        self.isVibrationEnabled = draft.isVibrationEnabled
+        self.isSoundEnabled = draft.isSoundEnabled
+        self.isRepeatEnabled = draft.isRepeatEnabled
+    }
+
+    // 기존 TimerModel에 Draft 값 반영 (편집 저장 시)
+    mutating func apply(_ draft: TimerDraft) {
+        self.title = draft.title
+        self.color = draft.color
+        self.totalTime = TimeInterval(draft.totalSeconds)
+        self.currentTime = TimeInterval(draft.totalSeconds)
+        self.isTickAlwaysVisible = draft.isTickAlwaysVisible
+        self.isVibrationEnabled = draft.isVibrationEnabled
+        self.isSoundEnabled = draft.isSoundEnabled
+        self.isRepeatEnabled = draft.isRepeatEnabled
     }
 }
