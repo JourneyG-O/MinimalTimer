180de482f4391c2428c1bed13d31bdf5443c4697
JourneyG-O <gojs2639@gmail.com>
2025-08-21 22:53:06 +0900

    🎨 UI: Center total time and position title at midpoint between circle top and center

--- MinimalTimer/MinimalTimer/Features/MainTimer/PreviewTimerView.swift
+++ MinimalTimer/MinimalTimer/Features/MainTimer/PreviewTimerView.swift
@@ -14,25 +14,34 @@ struct PreviewTimerView: View {
     let onSelected: () -> Void
 
     var body: some View {
-        ZStack {
-            Circle()
-                .fill(color)
+        GeometryReader { geo in
+            let side   = min(geo.size.width, geo.size.height)
+            let center = CGPoint(x: geo.size.width / 2, y: geo.size.height / 2)
+            let radius = side / 2
+            let titleY = center.y - radius / 2
 
-            VStack(spacing: 8) {
-                Text(title)
-                    .foregroundColor(Color(.systemBackground))
-                    .font(.system(size: 30, weight: .semibold, design: .rounded))
-                    .lineLimit(1)
-                    .minimumScaleFactor(0.8)
+            ZStack {
+                Circle()
+                    .fill(color)
+                    .frame(width: side, height: side)
+                    .position(center)
 
                 Text(totalTime.mmss)
+                    .font(.system(size: side * 0.22, weight: .bold, design: .rounded))
+                    .monospacedDigit()
                     .foregroundColor(Color(.systemBackground))
-                    .font(.system(size: 40, weight: .bold, design: .rounded))
+                    .position(center)
+
+                Text(title)
+                    .font(.system(size: side * 0.12, weight: .semibold, design: .rounded))
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.8)
+                    .foregroundColor(Color(.systemBackground).opacity(0.95))
+                    .position(x: center.x, y: titleY)
             }
-
-
+            .contentShape(Circle())
+            .onTapGesture(perform: onSelected)
         }
-        .onTapGesture(perform: onSelected)
+        .aspectRatio(1, contentMode: .fit)
     }
 }
-
